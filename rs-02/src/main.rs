4f755e790fad7682553a320bd52814fa9ad47caf
Jack Rose <user@jackrose.co.nz>
2021-12-02 18:29:51 +1100

    day 2: move more logic into `Position`

--- rs-02/src/main.rs
+++ rs-02/src/main.rs
@@ -3,65 +3,6 @@ use std::fs;
 use std::result;
 use std::str::FromStr;
 
-type Result<T> = result::Result<T, Box<dyn Error>>;
-
-#[derive(Debug, PartialEq)]
-enum Motion {
-    Up(i32),
-    Down(i32),
-    Forward(i32),
-}
-
-impl FromStr for Motion {
-    type Err = Box<dyn Error>;
-
-    fn from_str(s: &str) -> Result<Motion> {
-        let split_line: Vec<&str> = s.trim().split(' ').collect();
-        Ok(match split_line[..] {
-            ["up", x] => Motion::Up(x.parse().unwrap()),
-            ["down", x] => Motion::Down(x.parse().unwrap()),
-            ["forward", x] => Motion::Forward(x.parse().unwrap()),
-            _ => panic!("Unknown command"),
-        })
-    }
-}
-
-struct Position {
-    aim: i32,
-    horizontal: i32,
-    depth: i32,
-}
-
-impl Position {
-    fn new() -> Self {
-        Self {
-            aim: 0,
-            horizontal: 0,
-            depth: 0,
-        }
-    }
-    fn move_forward(mut self: Self, x: &i32) -> Self {
-        self.horizontal += x;
-        self
-    }
-    fn move_down(mut self: Self, x: &i32) -> Self {
-        self.depth += x;
-        self
-    }
-    fn move_up(mut self: Self, x: &i32) -> Self {
-        self.depth -= x;
-        self
-    }
-    fn raise_aim(mut self: Self, x: &i32) -> Self {
-        self.aim -= x;
-        self
-    }
-    fn lower_aim(mut self: Self, x: &i32) -> Self {
-        self.aim += x;
-        self
-    }
-}
-
 fn main() {
     let parsed_input = parse_input(&read_input());
     println!("part 1: {}", part_1(&parsed_input));
@@ -80,32 +21,107 @@ fn parse_input(input: &str) -> Vec<Motion> {
 }
 
 fn part_1(input: &Vec<Motion>) -> i32 {
-    let end_position: Position =
-        input
-            .iter()
-            .fold(Position::new(), |position, motion| match motion {
-                Motion::Forward(x) => position.move_forward(x),
-                Motion::Up(x) => position.move_up(x),
-                Motion::Down(x) => position.move_down(x),
-            });
+    let end_position = input
+        .iter()
+        .fold(Position1::new(), |position, motion| position.update(motion));
 
     end_position.horizontal * end_position.depth
 }
 
 fn part_2(input: &Vec<Motion>) -> i32 {
-    let end_position: Position =
-        input
-            .iter()
-            .fold(Position::new(), |position, motion| match motion {
-                Motion::Forward(x) => {
-                    let new_depth = position.aim * x;
-                    position.move_forward(x).move_down(&new_depth)
-                }
-                Motion::Up(x) => position.raise_aim(x),
-                Motion::Down(x) => position.lower_aim(x),
-            });
-
+    let end_position = input
+        .iter()
+        .fold(Position2::new(), |position, motion| position.update(motion));
     end_position.horizontal * end_position.depth
+}
+
+type Result<T> = result::Result<T, Box<dyn Error>>;
+
+#[derive(Debug, PartialEq)]
+enum Motion {
+    Up(i32),
+    Down(i32),
+    Forward(i32),
+}
+impl FromStr for Motion {
+    type Err = Box<dyn Error>;
+
+    fn from_str(s: &str) -> Result<Motion> {
+        let split_line: Vec<&str> = s.trim().split(' ').collect();
+        Ok(match split_line[..] {
+            ["up", x] => Motion::Up(x.parse().unwrap()),
+            ["down", x] => Motion::Down(x.parse().unwrap()),
+            ["forward", x] => Motion::Forward(x.parse().unwrap()),
+            _ => panic!("Unknown command"),
+        })
+    }
+}
+
+struct Position1 {
+    horizontal: i32,
+    depth: i32,
+}
+impl Position1 {
+    fn new() -> Self {
+        Self {
+            horizontal: 0,
+            depth: 0,
+        }
+    }
+    fn move_forward(mut self: Self, x: &i32) -> Self {
+        self.horizontal += x;
+        self
+    }
+    fn move_down(mut self: Self, x: &i32) -> Self {
+        self.depth += x;
+        self
+    }
+    fn move_up(mut self: Self, x: &i32) -> Self {
+        self.depth -= x;
+        self
+    }
+    fn update(self: Self, motion: &Motion) -> Self {
+        match motion {
+            Motion::Forward(x) => self.move_forward(x),
+            Motion::Up(x) => self.move_up(x),
+            Motion::Down(x) => self.move_down(x),
+        }
+    }
+}
+
+struct Position2 {
+    aim: i32,
+    horizontal: i32,
+    depth: i32,
+}
+impl Position2 {
+    fn new() -> Self {
+        Self {
+            aim: 0,
+            horizontal: 0,
+            depth: 0,
+        }
+    }
+    fn move_forward(mut self: Self, x: &i32) -> Self {
+        self.horizontal += x;
+        self.depth += self.aim * x;
+        self
+    }
+    fn raise_aim(mut self: Self, x: &i32) -> Self {
+        self.aim -= x;
+        self
+    }
+    fn lower_aim(mut self: Self, x: &i32) -> Self {
+        self.aim += x;
+        self
+    }
+    fn update(self: Self, motion: &Motion) -> Self {
+        match motion {
+            Motion::Forward(x) => self.move_forward(x),
+            Motion::Up(x) => self.raise_aim(x),
+            Motion::Down(x) => self.lower_aim(x),
+        }
+    }
 }
 
 #[cfg(test)]
