4853a5cdfd94172e52701af9b357cc83997ce6cb
Jack Rose <user@jackrose.co.nz>
2021-12-06 17:13:49 +1100

    day 6 part 2

--- rs-06/src/main.rs
+++ rs-06/src/main.rs
@@ -2,64 +2,39 @@ use std::fs;
 
 fn main() {
     println!("part 1: {}", find_school_size_at_time("input", 80));
-}
-
-#[derive(Debug)]
-struct Fish {
-    age: i32,
-}
-
-impl Fish {
-    fn new(age: i32) -> Self {
-        Self { age }
-    }
-
-    fn age(&mut self) -> bool {
-        self.age -= 1;
-        if self.age < 0 {
-            self.age = 6;
-            // returning true means the fish has given birth
-            true
-        } else {
-            false
-        }
-    }
+    println!("part 2: {}", find_school_size_at_time("input", 256));
 }
 
 struct School {
-    fish: Vec<Fish>,
+    fish_by_age: Vec<i64>,
 }
 
 impl School {
-    fn new(fish: Vec<i32>) -> Self {
+    fn new(ages: Vec<i64>) -> Self {
         Self {
-            fish: fish.into_iter().map(|age| Fish::new(age)).collect(),
+            fish_by_age: ages.iter().fold(vec![0; 9], |mut by_age, age| {
+                by_age[*age as usize] += 1;
+                by_age
+            }),
         }
     }
 
-    fn age(&mut self) {
-        let mut new_fish_count = 0;
-        for fish in &mut self.fish {
-            let new_fish = fish.age();
-            if new_fish {
-                new_fish_count += 1;
-            }
-        }
-        for _ in 0..new_fish_count {
-            self.fish.push(Fish::new(8));
-        }
+    fn tick(&mut self) {
+        let giving_birth_count = self.fish_by_age.remove(0);
+        self.fish_by_age.push(giving_birth_count);
+        self.fish_by_age[6] += giving_birth_count;
     }
 }
 
-fn find_school_size_at_time(filename: &str, time: i32) -> usize {
+fn find_school_size_at_time(filename: &str, time: i64) -> i64 {
     let mut school = School::new(parse_input(filename));
     for _ in 0..time {
-        school.age();
+        school.tick();
     }
-    school.fish.len()
+    school.fish_by_age.iter().sum()
 }
 
-fn parse_input(filename: &str) -> Vec<i32> {
+fn parse_input(filename: &str) -> Vec<i64> {
     fs::read_to_string(filename)
         .expect("file not found")
         .trim()
@@ -79,28 +54,6 @@ mod tests {
     }
 
     #[test]
-    fn test_fish_increase_age() {
-        let mut fish = Fish::new(1);
-
-        let new_fish = fish.age();
-        assert_eq!(fish.age, 0);
-        assert_eq!(new_fish, false);
-
-        let new_fish = fish.age();
-        assert_eq!(fish.age, 6);
-        assert_eq!(new_fish, true);
-    }
-
-    #[test]
-    fn test_school_increase_age() {
-        let mut school = School::new(vec![1, 2, 3]);
-        school.age();
-        assert_eq!(school.fish[0].age, 0);
-        assert_eq!(school.fish[1].age, 1);
-        assert_eq!(school.fish[2].age, 2);
-    }
-
-    #[test]
     fn test_part_1_sample() {
         assert_eq!(find_school_size_at_time("input.test", 80), 5934);
     }
@@ -108,5 +61,15 @@ mod tests {
     #[test]
     fn test_part_1_real() {
         assert_eq!(find_school_size_at_time("input", 80), 359344);
+    }
+
+    #[test]
+    fn test_part_2_sample() {
+        assert_eq!(find_school_size_at_time("input.test", 256), 26984457539);
+    }
+
+    #[test]
+    fn test_part_2_real() {
+        assert_eq!(find_school_size_at_time("input", 256), 1629570219571);
     }
 }
