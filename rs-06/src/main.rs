d6e6b6fe4d563ab239b121ad1a95f9bc1dd736ab
Jack Rose <user@jackrose.co.nz>
2021-12-06 17:44:37 +1100

    day 6: make a bit more OO

--- rs-06/src/main.rs
+++ rs-06/src/main.rs
@@ -1,46 +1,49 @@
 use std::fs;
+use std::str::FromStr;
 
 fn main() {
-    println!("part 1: {}", find_school_size_at_time("input", 80));
-    println!("part 2: {}", find_school_size_at_time("input", 256));
+    let school: School = fs::read_to_string("input.test").unwrap().parse().unwrap();
+    println!("part 1: {}", school.size_at_time(80));
+    println!("part 2: {}", school.size_at_time(256));
 }
 
+#[derive(Clone)]
 struct School {
     fish_by_age: Vec<i64>,
 }
 
-impl School {
-    fn new(ages: Vec<i64>) -> Self {
-        Self {
+impl FromStr for School {
+    type Err = String;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let ages: Vec<i64> = input
+            .trim()
+            .split(",")
+            .map(|l| l.parse().unwrap())
+            .collect();
+        Ok(School {
             fish_by_age: ages.iter().fold(vec![0; 9], |mut by_age, age| {
                 by_age[*age as usize] += 1;
                 by_age
             }),
-        }
+        })
     }
+}
 
+impl School {
     fn tick(&mut self) {
         let giving_birth_count = self.fish_by_age.remove(0);
         self.fish_by_age.push(giving_birth_count);
         self.fish_by_age[6] += giving_birth_count;
     }
-}
 
-fn find_school_size_at_time(filename: &str, time: i64) -> i64 {
-    let mut school = School::new(parse_input(filename));
-    for _ in 0..time {
-        school.tick();
+    fn size_at_time(&self, time: i64) -> i64 {
+        let mut school = self.clone();
+        for _ in 0..time {
+            school.tick();
+        }
+        school.fish_by_age.iter().sum()
     }
-    school.fish_by_age.iter().sum()
-}
-
-fn parse_input(filename: &str) -> Vec<i64> {
-    fs::read_to_string(filename)
-        .expect("file not found")
-        .trim()
-        .split(",")
-        .map(|l| l.parse().unwrap())
-        .collect()
 }
 
 #[cfg(test)]
@@ -49,27 +52,31 @@ mod tests {
 
     #[test]
     fn test_parse_input() {
-        let input = parse_input("input.test");
-        assert_eq!(input, vec![3, 4, 3, 1, 2]);
+        let school: School = fs::read_to_string("input.test").unwrap().parse().unwrap();
+        assert_eq!(school.fish_by_age, vec![0, 1, 1, 2, 1, 0, 0, 0, 0]);
     }
 
     #[test]
     fn test_part_1_sample() {
-        assert_eq!(find_school_size_at_time("input.test", 80), 5934);
+        let school: School = fs::read_to_string("input.test").unwrap().parse().unwrap();
+        assert_eq!(school.size_at_time(80), 5934);
     }
 
     #[test]
     fn test_part_1_real() {
-        assert_eq!(find_school_size_at_time("input", 80), 359344);
+        let school: School = fs::read_to_string("input").unwrap().parse().unwrap();
+        assert_eq!(school.size_at_time(80), 359344);
     }
 
     #[test]
     fn test_part_2_sample() {
-        assert_eq!(find_school_size_at_time("input.test", 256), 26984457539);
+        let school: School = fs::read_to_string("input.test").unwrap().parse().unwrap();
+        assert_eq!(school.size_at_time(256), 26984457539);
     }
 
     #[test]
     fn test_part_2_real() {
-        assert_eq!(find_school_size_at_time("input", 256), 1629570219571);
+        let school: School = fs::read_to_string("input").unwrap().parse().unwrap();
+        assert_eq!(school.size_at_time(256), 1629570219571);
     }
 }
