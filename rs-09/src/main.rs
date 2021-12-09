cb247710e9f749dab31ba379302c2d5d55147374
Jack Rose <user@jackrose.co.nz>
2021-12-09 18:05:44 +1100

    day 9 part 2

--- rs-09/src/main.rs
+++ rs-09/src/main.rs
@@ -1,19 +1,19 @@
-use std::collections::HashMap;
-use std::fs;
+use std::collections::{HashMap, HashSet};
+use std::{fs, usize};
 
 type Position = (usize, usize);
 type HeightMap = HashMap<Position, i32>;
 
 fn main() {
     println!("part 1: {}", part_1("input"));
+    println!("part 2: {}", part_2("input"));
 }
 
 fn part_1(filename: &str) -> i32 {
-    let input = get_input(filename);
-    input
-        .iter()
+    let map = get_input(filename);
+    map.iter()
         .map(|(pos, value)| {
-            if is_low_point(&input, pos) {
+            if is_low_point(&map, pos) {
                 value + 1
             } else {
                 0
@@ -22,24 +22,63 @@ fn part_1(filename: &str) -> i32 {
         .sum()
 }
 
-fn is_lower_than(map: &HeightMap, pos: &Position, other_pos: &Position) -> bool {
-    let value = map.get(pos).unwrap();
-    let other_value = map.get(other_pos);
-    if pos == other_pos {
-        // saturating_sub was performed, ignore this check
-        return true;
+fn part_2(filename: &str) -> usize {
+    let map = get_input(filename);
+    let low_points = map
+        .iter()
+        .filter(|(pos, _)| is_low_point(&map, pos))
+        .map(|(pos, _)| pos)
+        .collect::<Vec<&Position>>();
+
+    let basins: Vec<HashSet<Position>> =
+        low_points.iter().map(|pos| get_basin(&map, pos)).collect();
+
+    let mut basin_sizes: Vec<_> = basins.iter().map(|b| b.len()).collect();
+    basin_sizes.sort_unstable();
+    basin_sizes.iter().rev().take(3).product()
+}
+
+fn get_basin(map: &HeightMap, pos: &Position) -> HashSet<Position> {
+    let mut basin: HashSet<Position> = HashSet::new();
+    let mut unvisited: HashSet<Position> = HashSet::new();
+    unvisited.insert(*pos);
+
+    while !unvisited.is_empty() {
+        let unvisited_clone = unvisited.clone();
+        let next_to_visit = unvisited_clone.iter().next().unwrap();
+        unvisited.remove(next_to_visit);
+
+        let all_neighbours: HashSet<_> = get_neighbors(map, next_to_visit)
+            .iter()
+            .filter(|n| map.get(n).unwrap() != &9)
+            .cloned()
+            .collect();
+
+        let basin_clone = basin.clone();
+        let new_neighbours: HashSet<_> = all_neighbours.difference(&basin_clone).collect();
+        basin.extend(new_neighbours.clone().into_iter());
+        unvisited.extend(new_neighbours.clone().into_iter());
     }
-    match other_value {
-        Some(other_val) => value < other_val,
-        None => true,
-    }
+    basin
 }
 
 fn is_low_point(map: &HeightMap, pos: &Position) -> bool {
-    is_lower_than(map, pos, &(pos.0, pos.1.saturating_sub(1))) && // above
-    is_lower_than(map, pos, &(pos.0.saturating_sub(1), pos.1)) && // left
-    is_lower_than(map, pos, &(pos.0, pos.1 + 1)) && // below
-    is_lower_than(map, pos, &(pos.0 + 1, pos.1)) // right
+    get_neighbors(map, pos)
+        .iter()
+        .all(|other_pos| map.get(pos).unwrap() < map.get(other_pos).unwrap())
+}
+
+fn get_neighbors(map: &HeightMap, pos: &Position) -> Vec<Position> {
+    vec![
+        (pos.0, pos.1.saturating_sub(1)), // above
+        (pos.0.saturating_sub(1), pos.1), // left
+        (pos.0, pos.1 + 1),               // below
+        (pos.0 + 1, pos.1),               // right
+    ]
+    .iter()
+    .filter(|other_pos| *other_pos != pos && map.get(other_pos).is_some())
+    .cloned()
+    .collect()
 }
 
 fn get_input(filename: &str) -> HeightMap {
@@ -59,11 +98,22 @@ mod tests {
 
     #[test]
     fn test_get_input() {
-        let input = get_input("input.test");
-        assert_eq!(input.get(&(0, 0)), Some(&2)); // top left
-        assert_eq!(input.get(&(9, 0)), Some(&0)); // top right
-        assert_eq!(input.get(&(9, 4)), Some(&8)); // bottom right
-        assert_eq!(input.get(&(0, 4)), Some(&9)); // bottom left
+        let map = get_input("input.test");
+        assert_eq!(map.get(&(0, 0)), Some(&2)); // top left
+        assert_eq!(map.get(&(9, 0)), Some(&0)); // top right
+        assert_eq!(map.get(&(9, 4)), Some(&8)); // bottom right
+        assert_eq!(map.get(&(0, 4)), Some(&9)); // bottom left
+    }
+
+    #[test]
+    fn test_get_neighbors() {
+        let map = get_input("input.test");
+        assert_eq!(get_neighbors(&map, &(0, 0)), vec![(0, 1), (1, 0)]);
+        assert_eq!(
+            get_neighbors(&map, &(1, 1)),
+            vec![(1, 0), (0, 1), (1, 2), (2, 1)]
+        );
+        assert_eq!(get_neighbors(&map, &(9, 4)), vec![(9, 3), (8, 4)]);
     }
 
     #[test]
@@ -74,5 +124,15 @@ mod tests {
     #[test]
     fn test_part_1_real() {
         assert_eq!(part_1("input"), 566)
+    }
+
+    #[test]
+    fn test_part_2_sample() {
+        assert_eq!(part_2("input.test"), 1134)
+    }
+
+    #[test]
+    fn test_part_2_real() {
+        assert_eq!(part_2("input"), 891684)
     }
 }
