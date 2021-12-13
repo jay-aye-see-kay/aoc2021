f94515bd17acc9fe3967ea513e08aa27531ae993
Jack Rose <user@jackrose.co.nz>
2021-12-13 16:51:46 +1100

    day 13 part 1

--- rs-13/src/main.rs
+++ rs-13/src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 
+use std::fmt::{Display, Error, Formatter};
 use std::fs;
 
 fn main() {
@@ -8,7 +9,7 @@ fn main() {
 
 type Position = (usize, usize);
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Fold {
     X(usize),
     Y(usize),
@@ -47,6 +48,41 @@ fn get_input(filename: &str) -> Transparency {
     }
 }
 
+fn fold_transparency(initial_transparency: &Transparency) -> Transparency {
+    let mut new_folds = initial_transparency.folds.clone();
+    let active_fold = new_folds.remove(0);
+
+    let mut new_dots: Vec<_> = initial_transparency
+        .dots
+        .iter()
+        .map(|(x, y)| match active_fold {
+            Fold::X(axis) => {
+                if *x > axis {
+                    let distance_past_axis = x - axis;
+                    (axis - distance_past_axis, *y)
+                } else {
+                    (*x, *y)
+                }
+            }
+            Fold::Y(axis) => {
+                if *y > axis {
+                    let distance_past_axis = y - axis;
+                    (*x, axis - distance_past_axis)
+                } else {
+                    (*x, *y)
+                }
+            }
+        })
+        .collect();
+    new_dots.sort_unstable();
+    new_dots.dedup();
+
+    Transparency {
+        folds: new_folds,
+        dots: new_dots,
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -63,5 +99,19 @@ mod tests {
         assert_eq!(input.folds.len(), 2);
         assert_eq!(input.folds[0], Fold::Y(7));
         assert_eq!(input.folds[1], Fold::X(5));
+    }
+
+    #[test]
+    fn test_part_1_sample() {
+        let input = get_input("input.test");
+        let folded = fold_transparency(&input);
+        assert_eq!(folded.dots.len(), 17);
+    }
+
+    #[test]
+    fn test_part_1_real() {
+        let input = get_input("input");
+        let folded = fold_transparency(&input);
+        assert_eq!(folded.dots.len(), 592);
     }
 }
